1418618d243187c4eb5dd43856cf3a0fccd92b38
edoardopastori <edoardo.pastori@edu.unito.it>
2023-10-11 15:12:41 +0200

    aggiunti dati nell'sql della tabella degli utenti

--- api_server/dataMgr/user.sql
+++ api_server/dataMgr/user.sql
@@ -18,103 +18,110 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `fumetti`
+-- Database: `cinemaster`
 --
+
+
+CREATE DATABASE if not exists cinemaster;
+
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `categorie`
+-- Svuoto il database dalle tabelle esistenti
+--
+DROP TABLE if exists user_table;
+DROP TABLE if exists films;
+
+
+--
+-- Table structure for table `films`
 --
 
-CREATE TABLE `categorie` (
-  `idcat` int(11) NOT NULL,
-  `nomecat` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `descrizionecat` text COLLATE utf8mb4_unicode_ci NOT NULL
+CREATE TABLE films (
+    `id` int(11) NOT NULL,
+    `titolo` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `data_uscita` DATE,
+    `regista` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `durata` int,
+    `prezzo` float,
+    `categoria` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `descrizione` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `img` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `categorie`
+-- Table structure for table `user_table`
 --
 
-INSERT INTO `categorie` (`idcat`, `nomecat`, `descrizionecat`) VALUES
-(1, 'Tex', 'Collezione Tex classico'),
-(2, 'Dylan Dog', 'Tutto Dylan Dog'),
-(3, 'Topolino', 'I classici di Topolino e Paperino');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `prodotti`
---
-
-CREATE TABLE `prodotti` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `descrizione` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `prezzo` decimal(10,2) NOT NULL,
-  `cat_id` int(11) NOT NULL
+CREATE TABLE user_table (
+    `id` int(11) NOT NULL,
+    `nome` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `cognome` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `password` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `email` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `data_nascita` DATE,
+    `ruolo` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `logged` boolean
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Dumping data for table `prodotti`
---
 
-INSERT INTO `prodotti` (`id`, `nome`, `descrizione`, `prezzo`, `cat_id`) VALUES
-(1, 'L\'alba dei morti viventi', 'Dylan Dog numero 1 (1986)', '90.00', 2),
-(2, 'Il fantasma di Anna Never', 'Dylan Dog numero 4 (1987)', '30.00', 2),
-(3, 'Il castello della paura', 'Dylan Dog numero 16 (1988)', '10.00', 2),
-(4, 'La mano rossa', 'Tex numero 1 (1958)', '250.00', 1),
-(5, 'Fuorilegge', 'Tex numero 3 (1959)', '120.00', 1),
-(6, 'Il patto di sangue', 'Tex numero 7 (1960)', '40.00', 1),
-(7, 'Topolino e il mistero di Macchia', 'Topolino detective', '55.00', 3),
-(8, 'Paperinik il diabolico vendicatore', 'Storia di Paperinik', '35.00', 3),
-(9, 'Eta Beta e la spia', 'Storia di Eta Beta', '15.00', 3);
+
 
 --
--- Indexes for dumped tables
+-- Dumping data for table `films`
 --
 
---
--- Indexes for table `categorie`
---
-ALTER TABLE `categorie`
-  ADD PRIMARY KEY (`idcat`);
+INSERT INTO `films` (`id`,`titolo`,`data_uscita`,`regista`,`durata`,`prezzo`,`categoria`,`descrizione`,`img`) VALUES
+(1, 'Die Hart the Movie', '2023-03-03', 'Eric Appel', 92, 15, 'Azione', 'Kevin Hart, è impegnato in una missione dove sfida la morte per diventare una star di azione. E con un piccolo aiuto da parte di John Travolta, Nathalie Emmanuel e Josh Hartnett, potrebbe farcela.', 'die_hart.jpg'),
+(2, 'Unlocked', '2023-03-09', 'Kim Tae-joon', 100, 18, 'Drammatico', 'La vita di una donna viene stravolta dopo che un uomo pericoloso entra in possesso del suo cellulare smarrito e lo usa per seguire ogni sua mossa.', 'unlocked.jpg'),
+(3, 'Dog Gone', '2023-05-01', 'Stephen Herek', 88, 15, 'Per famiglie', 'Quando il suo amato cane scompare, un giovane si imbarca in una incredibile ricerca con i suoi genitori per trovarlo e dargli un farmaco salvavita.', 'dog_gone.jpg'),
+(4, 'True Spirit', '2023-08-10', 'Danny Ruhlmann', 96, 15, 'Azione', 'Una tenace adolescente australiana insegue i suoi sogni, e affronta le sue paure, mentre si accinge a diventare la più giovane persona a navigare in solitaria intorno al mondo.', 'true_spirit.jpg');
+
 
 --
--- Indexes for table `prodotti`
---
-ALTER TABLE `prodotti`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `prodotti_ibfk_1` (`cat_id`);
-
---
--- AUTO_INCREMENT for dumped tables
+-- Dumping data for table `user_table`
 --
 
---
--- AUTO_INCREMENT for table `categorie`
---
-ALTER TABLE `categorie`
-  MODIFY `idcat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+INSERT INTO `user_table` (`id`,`nome`,`cognome`,`password`,`email`,`data_nascita`,`ruolo`,`logged`) VALUES
+(1, 'Alice', 'Corvetto', 'alccrvtt', 'alice.corvetto@cmail.it', '1999-03-03 00:00:00', 'utente','0'),
+(2,'Marco', 'Donati', 'mrcdnt', 'marco.donati@cmail.it', '2001-05-05 00:00:00', 'utente','0'),
+(3,'Giovanni', 'Roselli', 'gvnnrsll', 'giovanni_roselli@cmail.it', '2000-04-04 00:00:00', 'utente','0'),
+(4,'Luca', 'Grazioli', 'cinemaster_77456', 'admin_luca@cinemaster.com', '1970-01-01 00:00:00', 'admin','0'),
+(5,'Sara', 'Flori', 'cinemaster_33245', 'admin_sara@cinemaster.it', '1970-01-01 00:00:00', 'admin','0');
 
 --
--- AUTO_INCREMENT for table `prodotti`
+-- Indexes for table `user_table`
 --
-ALTER TABLE `prodotti`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+ALTER TABLE `user_table`
+  ADD PRIMARY KEY (`id`);
+
 
 --
--- Constraints for dumped tables
+-- Indexes for table `films`
 --
+ALTER TABLE `films`
+  ADD PRIMARY KEY (`id`);
 
 --
--- Constraints for table `prodotti`
+-- AUTO_INCREMENT for table `user_table`
 --
-ALTER TABLE `prodotti`
-  ADD CONSTRAINT `prodotti_ibfk_1` FOREIGN KEY (`cat_id`) REFERENCES `categorie` (`idcat`);
-COMMIT;
+ALTER TABLE `user_table`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+
+
+--
+-- AUTO_INCREMENT for table `films`
+--
+ALTER TABLE `films`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+
